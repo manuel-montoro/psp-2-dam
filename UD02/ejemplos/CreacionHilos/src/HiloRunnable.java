3dd7d53b9867cb95bbc2432b1fa4e6073a38bd05
Manuel Montoro <erlukasgrijander@gmail.com>
2022-11-07 19:59:27 +0100

    Corregido error en ejemplo de creación de hilos con Runnable

--- UD02/ejemplos/CreacionHilos/src/HiloRunnable.java
+++ UD02/ejemplos/CreacionHilos/src/HiloRunnable.java
@@ -9,13 +9,15 @@ public class HiloRunnable implements Runnable{
 
   @Override
   public void run() {
-    System.out.println("Hola, soy el hilo " + idHilo);
+    System.out.println("Hola, soy el hilo " + Thread.currentThread().getName());
   }
   
   public static void main(String[] args) {
     
-    Thread hilo1 = new Thread(new HiloThread(1));
-    Thread hilo2 = new Thread(new HiloThread(2));
+    Thread hilo1 = new Thread(new HiloRunnable(1));
+    hilo1.setName("Hilo 1");
+    Thread hilo2 = new Thread(new HiloRunnable(2));
+    hilo2.setName("Otro hilo");
     
     hilo1.start();
     hilo2.start();
